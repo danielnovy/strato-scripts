62aff23a092d1f8c543622225ce937e3205fae2e
Ryan Reich <ryan.reich@gmail.com>
2015-10-22 19:44:35 -0400

    Finish setup-rhel7.sh

--- setup-rhel7.sh
+++ setup-rhel7.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+# Get screen
+sudo yum -y install screen
+
 # Get stack
 curl -sSL https://s3.amazonaws.com/download.fpcomplete.com/centos/7/fpco.repo | sudo tee /etc/yum.repos.d/fpco.repo
 sudo yum -y install stack
 
+# Get git
+sudo yum -y install git
+
 # Get mgit
-git clone git@github.com:blockapps/mgit
+git clone http://github.com/blockapps/mgit
 cd mgit
 stack setup
 stack install
 
 # Get ethereumH
 cd
-mgit clone git@github.com:blockapps/ethereumH -b develop
+mgit clone http://github.com/blockapps/ethereumH -b develop
 cd ethereumH/ethereum-vm
-git rebase develop deployment-patches
+git merge origin/deployment-patches
 
 # Get API certificates
-echo "Enter azure password to get certificates"
-scp 'ryanr@azure.blockapps.net:~/ethereumH/hserver-eth/{priv,{certificate,key}.pem}' .
+cp ~/{priv,{certificate,key}.pem} ~/ethereumH/hserver-eth
 
 # Get solc
 cd
-echo "Enter azure password to get solc"
-scp ryanr@azure.blockapps.net:~/solc.tar.gz .
 tar xvf solc.tar.gz 
 sed -i "s@$$HOME@$HOME@g" ~/.local/bin/solc
 
@@ -32,19 +35,20 @@ sed -i "s@$$HOME@$HOME@g" ~/.local/bin/solc
 stack install happy alex
 
 ## PostgreSQL
-sudo yum install http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-redhat94-9.4-2.noarch.rpm
-# sudo yum -y install postgresql
-# sudo yum -y install postgresql-devel
-# sudo yum -y install postgresql94-server
+sudo yum -y install http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-redhat94-9.4-2.noarch.rpm
+sudo yum -y install postgresql
+sudo yum -y install postgresql-devel
+sudo yum -y install postgresql94-server
+sudo /usr/pgsql-9.4/bin/postgresql94-setup initdb
 sudo systemctl enable postgresql-9.4.service
 sudo systemctl start postgresql-9.4.service
 
 ## LevelDB
-sudo yum install ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/7/x86_64/l/leveldb-1.12.0-5.el7.x86_64.rpm
-sudo yum install ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/7/x86_64/l/leveldb-devel-1.12.0-5.el7.x86_64.rpm
+sudo yum -y install ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/7/x86_64/l/leveldb-1.12.0-5.el7.x86_64.rpm
+sudo yum -y install ftp://ftp.pbone.net/mirror/download.fedora.redhat.com/pub/fedora/epel/7/x86_64/l/leveldb-devel-1.12.0-5.el7.x86_64.rpm
 
 ## ZLib
-sudo yum install zlib-devel
+sudo yum -y install zlib-devel
 
 # Build
 cd ~/ethereumH
@@ -53,7 +57,7 @@ stack install
 # Set up databases
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'api';"
 sudo -u postgres createdb eth 2> /dev/null || echo "database already exists"
-sudo cat >/var/lib/pgsql/9.4/data/pg_hba.conf <<EOF
+cat <<EOF | sudo tee /var/lib/pgsql/9.4/data/pg_hba.conf >/dev/null
 # TYPE  DATABASE        USER            ADDRESS                 METHOD
 
 # "local" is for Unix domain socket connections only
@@ -68,3 +72,8 @@ cd ~/ethereumH/ethereum-data-sql
 mv genesis.json genesis-real.json
 ln -s stablenetGenesis.json genesis.json
 ethereum-setup
+
+cd
+cp ~/ethereumH/ethereum-conf/ethconf.yaml ~/.ethereumH
+nohup ~/ethereumH/ethereum-build/startAPI.sh
+nohup ~/ethereumH/ethereum-build/startEVM.sh
